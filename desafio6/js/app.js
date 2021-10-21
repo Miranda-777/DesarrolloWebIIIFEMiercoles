6b72aeb612724927b4cbc2a6c1fa694c8b0bf120
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-10-21 07:49:02 -0300

    def 6

--- desafio6/js/app.js
+++ desafio6/js/app.js
@@ -1,25 +1,29 @@
-let miDiv = document.querySelector("#principal")
-let pokemones = []
+let miDiv = document.querySelector("#principal");
+let pokemones = [];
 
-
-const cargarListado =()=>{
-    let url ="https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-    fetch(url)
-    .then((response)=>{
-        return response.json();
+const cargarListado = () => {
+  let url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+  fetch(url)
+    .then((response) => {
+      return response.json();
     })
     //es una promesa
-    .then((objetoJson)=>{
-        const pokms = objetoJson.results;
-        console.log(pokms);
-        pokemones= pokms;
-        pokms.forEach((p)=>{
-            miDiv.innerHTML+=`
-            <button class="btn btn-primary"> ${p.name} </button>`
+    .then((objetoJson) => {
+      const pokms = objetoJson.results;
+      console.log(pokms);
 
-        })
-    })
-}
+      pokemones = pokms;
+      pokms.forEach((p) => {
+        miDiv.innerHTML+=`
+        <button class='btn btn-primary' onclick="crearImagen('${p.url}')"> ${p.name}</button>`;
+        //    `<button class="btn btn-primary"> ${p.name} </button>`
+      });
+    });
+};
 
 cargarListado();
 
+const crearImagen = (url) => {
+  let contenedor = document.getElementById("contenedor");
+  console.log()
+};
