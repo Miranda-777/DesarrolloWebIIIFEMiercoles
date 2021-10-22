5c210c4d1ca34759e2ee990daf2f6061ff1d900f
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-10-22 18:53:37 -0300

    desaf 6

--- desafio6/js/app.js
+++ desafio6/js/app.js
@@ -14,7 +14,7 @@ const cargarListado = () => {
 
       pokemones = pokms;
       pokms.forEach((p) => {
-        miDiv.innerHTML+=`
+        miDiv.innerHTML += `
         <button class='btn btn-primary' onclick="crearImagen('${p.url}')"> ${p.name}</button>`;
         //    `<button class="btn btn-primary"> ${p.name} </button>`
       });
@@ -25,5 +25,36 @@ cargarListado();
 
 const crearImagen = (url) => {
   let contenedor = document.getElementById("contenedor");
-  console.log()
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((obj) => {
+      console.log(obj);
+      cargarCarta(obj)
+    });
+};
+
+const cargarCarta = (pk) => {
+  let contenedor = document.getElementById("cont");
+  let mov = pk.moves
+  let dance = ""
+  mov.forEach((p)=> {
+    dance += `<option>${p.move.name}</option>`
+
+  })
+  let plantilla = `
+  <div class="card" style="width: 18rem;">
+  <img src="${pk.sprites.front_default}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${pk.name}</h5>
+    <p class="card-text"></p>
+    <select class="form-select">
+      ${dance}
+    </select>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+;
+contenedor.innerHTML=plantilla;
 };
